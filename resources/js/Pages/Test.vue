6e79f3181dc76fa05b8b6dc3398dbf19c5d52f5f
SolwDawg <sondang24.sh@gmail.com>
2024-03-31 19:01:04 +0700

    Init project

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello from Test.vue!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    // Your component options
+};
+</script>
+
+<style scoped>
+/* Your component styles */
+</style>
